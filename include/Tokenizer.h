18047ca139e977291de1792253281f917194f3c6
kezi <me@kez.io>
2019-01-17 11:51:33 -0500

    Use C++11; Add constants for language tokens; Parse delimiters

--- include/Tokenizer.h
+++ include/Tokenizer.h
@@ -8,10 +8,17 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
 
 class Tokenizer {
 private:
-  std::string Delimiter;
+  static const std::string Delimiter;
+  static const std::array<std::string, 30> LanguageTokens;
+  static const int IntegerTokenRepresentation = 31;
+  static const int IdentifierTokenRepresentation = 32;
+  static const int EOFTokenRepresentation = 33;
+  static const int IdentifierMaxLength = 8;
+
   std::vector <std::string> Tokens;
   int CurrentTokenPosition;
   std::ifstream InputFile;
