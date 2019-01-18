42b353563e6ade40c6b3cb29e111e1f58bc444ee
kezi <me@kez.io>
2019-01-18 08:33:11 -0500

    Update error handling

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -13,12 +13,15 @@ int main(int argc, char** argv) {
     std::cerr << "Please specify a file name." << std::endl;
     exit(1);
   }
-  Tokenizer tokenizer (argv[argc - 1]);
-
-  while (!tokenizer.isEOF()) {
-    std::string token = tokenizer.currentToken();
-    int tokenNumber = tokenizer.numberForToken(token);
-    std::cout << tokenNumber << std::endl;
-    tokenizer.nextToken();
+  try {
+    Tokenizer tokenizer (argv[argc - 1]);
+    while (!tokenizer.isEOF()) {
+      std::string token = tokenizer.currentToken();
+      int tokenNumber = tokenizer.numberForToken(token);
+      std::cout << tokenNumber << std::endl;
+      tokenizer.nextToken();
+    }
+  } catch (std::string error) {
+    std::cerr << error << std::endl;
   }
 }
